7bd8c8494c258025a13ad3f997de9902c32b8e69
Thyag <thyagarajulunaidu@yahoo.com>
2016-03-21 18:55:44 -0700

    added sauce

--- test/conf.js
+++ test/conf.js
@@ -1,6 +1,8 @@
 exports.config = {
   framework: 'mocha',
-  seleniumAddress: 'http://2601:641:c001:73b0:b5e0:5d5f:cef3:4444/wd/hub',
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  seleniumAddress: 'http://192.168.0.26:4444/wd/hub',
   specs: ['todo-spec.js'],
   capabilities: {
   'browserName': 'firefox' // or 'safari'
