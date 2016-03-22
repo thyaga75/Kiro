1745f820ff6e633b6c85bd662faf2091affa7ba6
Thyag <thyagarajulunaidu@yahoo.com>
2016-03-21 20:28:07 -0700

    added version for TAP

--- test/conf.js
+++ test/conf.js
@@ -2,7 +2,7 @@ exports.config = {
   framework: 'mocha',
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
-  seleniumAddress: 'http://192.168.0.26:4444/wd/hub',
+  //seleniumAddress: 'http://192.168.0.26:4444/wd/hub',
   specs: ['todo-spec.js'],
   capabilities: {
   'browserName': 'firefox' // or 'safari'
