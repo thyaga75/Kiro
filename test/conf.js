8db90260b4f31a140bebd2839e29e9f4728624df
Thyag <thyagarajulunaidu@yahoo.com>
2016-03-21 17:52:33 -0700

    changed selenium address

--- test/conf.js
+++ test/conf.js
@@ -1,8 +1,8 @@
 exports.config = {
   framework: 'mocha',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://2601:641:c001:73b0:b5e0:5d5f:cef3:4444/wd/hub',
   specs: ['todo-spec.js'],
   capabilities: {
-    'browserName': 'firefox' // or 'safari'
+  'browserName': 'firefox' // or 'safari'
 	}
 };
